9f24cafc32fee1c0fec6306a45ae648f16dcd690
Taras <taras.shyshatskyi.25@kse.org.ua>
2026-06-19 14:37:02 +0300

    V0.0.0.0.0.1 created

--- math_magic.py
+++ math_magic.py
@@ -1,3 +1,6 @@
 a=int(input("Insert a number"))
 print(f"Your number is {a}")
-print(f"Your number is not {-a}")
+if a==0:
+    print(f"YOur number is not 1")
+else:
+    print(f"Your number is not {-a}")
